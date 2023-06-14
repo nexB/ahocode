19b47912951193d18450d8b2abe6668de93bb994
Jay <jaykumar20march@gmail.com>
2023-06-14 10:23:50 +0530

    fix: fixed the keys and values method
    - Fixed the logic for keys() and values() method, the logics were earlier interchanged.
    
    Signed-off-by: 35C4n0r <jaykumar20march@gmail.com>

--- src/ahocode.py
+++ src/ahocode.py
@@ -174,7 +174,7 @@ class Automaton:
         offset = 0
         if (start and end) is not None:
             offset = start
-            string = string[start:end + 1]
+            string = string[start:end]
         for idx, symbol in enumerate(string):
             current_state = current_state.transitions.get(
                 symbol, zero_state.transitions.get(symbol, zero_state))
@@ -205,13 +205,36 @@ class Automaton:
                 return default
         return current_state.value
 
+    def find_all(self, string, callback, start=None, end=None):
+        """
+        find_all(string, callback, [start, [end]])
+
+        Perform the Aho-Corasick search procedure using the provided
+        input string and iterate over the matching tuples
+        (end_index, value) for keys found in string. Invoke the
+        callback callable for each matching tuple.
+
+        The callback callable must accept two positional arguments:
+        - end_index is the end index in the input string where a
+        trie key string was found. - value is the value associated
+        with the found key string.
+
+        The start and end optional arguments can be used to limit
+        the search to an input string slice as in string[start:end].
+
+        Equivalent to a loop on iter() calling a callable at each
+        iteration.
+        """
+        for idx, value in self.iter(string, start, end):
+            callback(idx, value)
+
     def values(self, prefix=None, wildcard=None, how=None):
         for key, value in self.items():
-            yield key
+            yield value
 
     def keys(self, prefix=None, wildcard=None, how=None):
         for key, value in self.items():
-            yield value
+            yield key
 
     def items(self, prefix=None, wildcard=None, how=None):
         processed = set()
